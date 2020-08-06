07bbebe02b1eb996fd30d79280123fc6c767e909
Facundo Varela <cufa.varela@gmail.com>
2020-08-06 16:15:52 -0300

    creation env & data

---
+++ stock.js
@@ -0,0 +1,32 @@
+export default {
+    stock: [
+        {
+            _id: "1",
+            marca: "Peugeot",
+            modelo: "206",
+            puertas: 4,
+            precio: 200.000,   /* FALTAN DECIMALES */
+        },
+        {
+            _id: "2",
+            marca: "Peugeot",
+            modelo: "208",
+            puertas: 5,
+            precio: 250.000,   /* FALTAN DECIMALES */
+        },
+        {
+            _id: "3",
+            marca: "Honda",
+            modelo: "Titan",
+            cilindrada: "125c",
+            precio: 60.000,   /* FALTAN DECIMALES */
+        },
+        {
+            _id: "4",
+            marca: "Yamaha",
+            modelo: "YBR",
+            cilindrada: "160c",
+            precio: 80.500,   /* FALTAN DECIMALES   0,5 */
+        },
+    ]
+}
